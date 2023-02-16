363444169bb8b069a8b2fa5ac770cc022dd52266
Marcos Guerreiro Rebelo <37541973+marcosrebelo97@users.noreply.github.com>
2023-02-16 19:09:50 -0400

    Update AttornatusApplication.java

--- src/main/java/com/desafiobackend/attornatus/AttornatusApplication.java
+++ src/main/java/com/desafiobackend/attornatus/AttornatusApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan(basePackages = {"com.desafiobackend.attornatus"}) // add this so the spring boot context knows where to look for entities
-
 public class AttornatusApplication {
 
 	public static void main(String[] args) {
